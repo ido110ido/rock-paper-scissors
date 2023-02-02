390afe27c0d6df47e5d0a2be75044933a7f0ef95
Ido Atias <ido110ido@gmail.com>
2023-02-02 17:51:35 +0200

    finle submit project

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var rules = document.getElementById("rules");
 var closeRules = document.getElementById("close");
+//gamepieces**********************************************************************
 var pickAType = document.getElementById("pickAType");
 var paper = document.getElementById("paper");
 var scissors = document.getElementById("scissors");
 var rock = document.getElementById("rock");
+var lizar = document.getElementById("lizard");
+var spock = document.getElementById("spock");
+//*************************************************************************** *
 var youWin = document.getElementById("youWin");
 var playAgain = document.getElementById("playAgain");
 var scoreCounter = document.getElementById("scoreCounter");
@@ -23,35 +27,63 @@ rules === null || rules === void 0 ? void 0 : rules.addEventListener("click", fu
 closeRules === null || closeRules === void 0 ? void 0 : closeRules.addEventListener("click", function () {
     popUpRules.style.display = "none";
 });
+//definig the types and lisene to press
 var GamePiece;
 (function (GamePiece) {
     GamePiece["rock"] = "rock";
     GamePiece["paper"] = "paper";
     GamePiece["scissors"] = "scissors";
+    GamePiece["lizard"] = "lizard";
+    GamePiece["spock"] = "spock";
 })(GamePiece || (GamePiece = {}));
 //chosing a type
 paper === null || paper === void 0 ? void 0 : paper.addEventListener("click", function () {
-    animatedResult({ name: GamePiece.paper, canWin: [GamePiece.rock] });
+    animatedResult({
+        name: GamePiece.paper,
+        canWin: [GamePiece.rock, GamePiece.spock]
+    });
     console.log(GamePiece.paper);
 });
 scissors === null || scissors === void 0 ? void 0 : scissors.addEventListener("click", function () {
-    animatedResult({ name: GamePiece.scissors, canWin: [GamePiece.paper] });
+    animatedResult({
+        name: GamePiece.scissors,
+        canWin: [GamePiece.paper, GamePiece.lizard]
+    });
     console.log(GamePiece.scissors);
 });
 rock === null || rock === void 0 ? void 0 : rock.addEventListener("click", function () {
-    animatedResult({ name: GamePiece.rock, canWin: [GamePiece.scissors] });
+    animatedResult({
+        name: GamePiece.rock,
+        canWin: [GamePiece.scissors, GamePiece.lizard]
+    });
     console.log(GamePiece.rock);
 });
+lizar === null || lizar === void 0 ? void 0 : lizar.addEventListener("click", function () {
+    animatedResult({
+        name: GamePiece.lizard,
+        canWin: [GamePiece.spock, GamePiece.paper]
+    });
+    console.log(GamePiece.lizard);
+});
+spock === null || spock === void 0 ? void 0 : spock.addEventListener("click", function () {
+    animatedResult({
+        name: GamePiece.spock,
+        canWin: [GamePiece.scissors, GamePiece.rock]
+    });
+    console.log(GamePiece.spock);
+});
+//press to play again
 playAgain === null || playAgain === void 0 ? void 0 : playAgain.addEventListener("click", function () {
     window.location.reload();
 });
+//present result in an animated way
 var animatedResult = function (userSelect) {
     pickAType === null || pickAType === void 0 ? void 0 : pickAType.style.display = "none";
     resultAnimated === null || resultAnimated === void 0 ? void 0 : resultAnimated.style.display = "flex";
     var title = document.createElement("h3");
     title.innerText = "You Picked";
     var selectedAvatar = document.createElement("div");
-    selectedAvatar.className = "circle ".concat(userSelect.name, " avatar");
+    selectedAvatar.className = "circleResult ".concat(userSelect.name, " avatar");
     var imegeAvatar = document.createElement("img");
     imegeAvatar.src = "./images/icon-".concat(userSelect.name, ".svg");
     selectedAvatar.appendChild(imegeAvatar);
@@ -63,9 +95,12 @@ var animatedResult = function (userSelect) {
     setTimeout(function () {
         var imegeComputer = document.createElement("img");
         imegeComputer.src = "./images/icon-".concat(computer, ".svg");
-        computerPick === null || computerPick === void 0 ? void 0 : computerPick.className = "circle ".concat(computer, " avatar");
+        computerPick === null || computerPick === void 0 ? void 0 : computerPick.className = "circleResult ".concat(computer, " avatar");
         computerPick === null || computerPick === void 0 ? void 0 : computerPick.appendChild(imegeComputer);
         setTimeout(function () {
+            if (window.outerWidth > 600) {
+                resultAnimated === null || resultAnimated === void 0 ? void 0 : resultAnimated.style.gap = "20vw";
+            }
             youWin === null || youWin === void 0 ? void 0 : youWin.style.display = "flex";
             presentResult === null || presentResult === void 0 ? void 0 : presentResult.innerText = textResult(userSelect, computer);
         }, 1000);
@@ -75,6 +110,7 @@ var animatedResult = function (userSelect) {
 var computerPicker = function () {
     var randomNum = Math.floor(Math.random() * Object.keys(GamePiece).length);
     var e = Object.values(GamePiece)[randomNum];
+    console.log("computer selected: ".concat(e));
     return GamePiece[e];
 };
 //cheack if user wins
@@ -82,6 +118,7 @@ var cheackWinner = function (userSelect, computerSelect) {
     var result = userSelect.canWin.reduce(function (result, crr) { return (crr === computerSelect ? true : result); }, false);
     return result;
 };
+//write how win to the screen and updat result counter
 var textResult = function (userSelect, computer) {
     if (computer === userSelect.name) {
         return " IT'S A TIE";
--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 const rules = document.getElementById("rules") as HTMLElement | null;
 const closeRules = document.getElementById("close") as HTMLElement | null;
-
+//gamepieces**********************************************************************
 const pickAType = document.getElementById("pickAType") as HTMLElement | null;
 const paper = document.getElementById("paper") as HTMLElement | null;
 const scissors = document.getElementById("scissors") as HTMLElement | null;
 const rock = document.getElementById("rock") as HTMLElement | null;
-
+const lizar = document.getElementById("lizard") as HTMLElement | null;
+const spock = document.getElementById("spock") as HTMLElement | null;
+//*************************************************************************** *
 const youWin = document.getElementById("youWin") as HTMLElement | null;
 const playAgain = document.getElementById("playAgain") as HTMLElement | null;
 const scoreCounter = document.getElementById(
@@ -35,10 +37,14 @@ rules?.addEventListener("click", () => {
 closeRules?.addEventListener("click", () => {
   popUpRules.style.display = "none";
 });
+
+//definig the types and lisene to press
 enum GamePiece {
   rock = "rock",
   paper = "paper",
   scissors = "scissors",
+  lizard = "lizard",
+  spock = "spock",
 }
 interface ITypeOfGamePiece {
   name: GamePiece;
@@ -46,27 +52,52 @@ interface ITypeOfGamePiece {
 }
 //chosing a type
 paper?.addEventListener("click", () => {
-  animatedResult({ name: GamePiece.paper, canWin: [GamePiece.rock] });
+  animatedResult({
+    name: GamePiece.paper,
+    canWin: [GamePiece.rock, GamePiece.spock],
+  });
   console.log(GamePiece.paper);
 });
 scissors?.addEventListener("click", () => {
-  animatedResult({ name: GamePiece.scissors, canWin: [GamePiece.paper] });
+  animatedResult({
+    name: GamePiece.scissors,
+    canWin: [GamePiece.paper, GamePiece.lizard],
+  });
   console.log(GamePiece.scissors);
 });
 rock?.addEventListener("click", () => {
-  animatedResult({ name: GamePiece.rock, canWin: [GamePiece.scissors] });
+  animatedResult({
+    name: GamePiece.rock,
+    canWin: [GamePiece.scissors, GamePiece.lizard],
+  });
   console.log(GamePiece.rock);
 });
+lizar?.addEventListener("click", () => {
+  animatedResult({
+    name: GamePiece.lizard,
+    canWin: [GamePiece.spock, GamePiece.paper],
+  });
+  console.log(GamePiece.lizard);
+});
+spock?.addEventListener("click", () => {
+  animatedResult({
+    name: GamePiece.spock,
+    canWin: [GamePiece.scissors, GamePiece.rock],
+  });
+  console.log(GamePiece.spock);
+});
+//press to play again
 playAgain?.addEventListener("click", () => {
   window.location.reload();
 });
+//present result in an animated way
 const animatedResult = (userSelect: ITypeOfGamePiece) => {
   pickAType?.style.display = "none";
   resultAnimated?.style.display = "flex";
   let title: HTMLElement = document.createElement("h3");
   title.innerText = "You Picked";
   let selectedAvatar: HTMLDivElement = document.createElement("div");
-  selectedAvatar.className = `circle ${userSelect.name} avatar`;
+  selectedAvatar.className = `circleResult ${userSelect.name} avatar`;
   let imegeAvatar: HTMLImageElement = document.createElement("img");
   imegeAvatar.src = `./images/icon-${userSelect.name}.svg`;
   selectedAvatar.appendChild(imegeAvatar);
@@ -78,9 +109,12 @@ const animatedResult = (userSelect: ITypeOfGamePiece) => {
   setTimeout(() => {
     let imegeComputer: HTMLImageElement = document.createElement("img");
     imegeComputer.src = `./images/icon-${computer}.svg`;
-    computerPick?.className = `circle ${computer} avatar`;
+    computerPick?.className = `circleResult ${computer} avatar`;
     computerPick?.appendChild(imegeComputer);
     setTimeout(() => {
+      if (window.outerWidth > 600) {
+        resultAnimated?.style.gap = "20vw";
+      }
       youWin?.style.display = "flex";
       presentResult?.innerText = textResult(userSelect, computer);
     }, 1000);
@@ -92,6 +126,8 @@ const computerPicker = (): GamePiece => {
     Math.random() * Object.keys(GamePiece).length
   );
   let e: string = Object.values(GamePiece)[randomNum];
+  console.log(`computer selected: ${e}`);
+
   return GamePiece[e];
 };
 //cheack if user wins
@@ -105,6 +141,7 @@ const cheackWinner = (
   );
   return result;
 };
+//write how win to the screen and updat result counter
 const textResult = (
   userSelect: ITypeOfGamePiece,
   computer: GamePiece
